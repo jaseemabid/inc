4efe04f6b050e6fd013757196ad01169ce3023b8
Jaseem Abid <jaseemabid@gmail.com>
2019-12-18 14:28:44 +0530

    Modify the default signal handler to work with stack overflows
    This turned out to be a whole lot more complicated than I expected, and without
    this rethinkdb blog there is no way I would have figured this out!
    
    https://rethinkdb.com/blog/handling-stack-overflow-on-custom-stacks
    
    The Rust cli is still broken and wont report errors from the child process
    correctly, but running the executable directly shows the errors as expected.
    
    ```
    $ echo "(let ((f (lambda (x) (if (zero? x) 42 (f (dec x)))))) (f 1000000))" | cargo run -q
    
    -- No Output -- :(
    ```
    
    ```
    $ DEBUG=1 LD_LIBRARY_PATH=./target/debug ./inc
    The glorious incremental compiler
    
    Program segmentation faulted due to invalid memory access.
    Signal number        : 11
    SIGSEGV at address   : 0x7ffe22426fe8
    [1]    70004 abort (core dumped)  DEBUG=1 LD_LIBRARY_PATH=./target/debug ./inc
    ```
    
    Authored-at: 3rd Wave, Indiranagar

--- rs/runtime.c
+++ rs/runtime.c
@@ -10,10 +10,33 @@
 // Explicitly link to the assembly entry point
 extern int64_t init(int64_t*) __attribute__((noinline));
 
+// Turns out writing a signal handler that can handle a segfault due to stack
+// overflow isn't that simple. See the rethinkdb blog for details.
+//
+// https://rethinkdb.com/blog/handling-stack-overflow-on-custom-stacks/
 void set_handler(void (*handler)(int, siginfo_t *, void *)) {
     struct sigaction action;
-    action.sa_flags = SA_SIGINFO;
+    action.sa_flags = SA_SIGINFO|SA_STACK;
     action.sa_sigaction = handler;
+
+    // By default, when a signal is delivered, its handler is called on the same
+    // stack where the program was running. But if the signal is due to stack
+    // overflow, then attempting to execute the handler will cause a second
+    // segfault. Linux is smart enough not to send this segfault back to the
+    // same signal handler, which would prevent an infinite cascade of
+    // segfaults. Instead, in effect, the signal handler does not work.
+
+    // To make it work, we have to provide an alternate stack to execute the
+    // signal handler on. The system call to install this stack is called
+    // sigaltstack. As a parameter, it takes a stack_t, which consists of a
+    // pointer to the base of the stack, the size of the stack, and some flags
+    // that aren’t relevant for our purposes.
+    stack_t segv_stack;
+    int SEGV_STACK_SIZE = 4096;
+    segv_stack.ss_sp = valloc(SEGV_STACK_SIZE);
+    segv_stack.ss_flags = 0;
+    segv_stack.ss_size = SEGV_STACK_SIZE;
+    sigaltstack(&segv_stack, NULL);
 
     if (sigaction(SIGSEGV, &action, NULL) == -1) {
         perror("sigsegv: sigaction");
@@ -22,7 +45,7 @@ void set_handler(void (*handler)(int, siginfo_t *, void *)) {
 }
 
 void handler(int signo, siginfo_t *info, __attribute__((unused)) void *extra) {
-    printf("Program crashed due to unexpected error \n");
+    printf("Segmentation fault due to invalid memory access \n");
     printf("Signal number        : %d \n", signo);
     printf("SIGSEGV at address   : 0x%lx \n", (long)info->si_addr);
     abort();
